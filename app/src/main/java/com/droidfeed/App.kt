72b5a84463f9480ea09ff72a6897427d1a7347ac
dgngulcan <dgngulcan@gmail.com>
2018-03-19 07:58:25 -0400

    add app open count tracking

--- app/src/main/java/com/droidfeed/App.kt
+++ app/src/main/java/com/droidfeed/App.kt
@@ -2,7 +2,9 @@ package com.droidfeed
 
 import android.app.Activity
 import android.app.Application
+import android.content.SharedPreferences
 import com.droidfeed.di.DaggerAppComponent
+import com.droidfeed.util.appOpenCount
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -17,17 +19,22 @@ class App : Application(), HasActivityInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
+    @Inject
+    lateinit var sharedPrefs: SharedPreferences
+
     override fun onCreate() {
         super.onCreate()
         initDagger()
+
+        sharedPrefs.appOpenCount += 1
     }
 
     private fun initDagger() {
         DaggerAppComponent
-                .builder()
-                .application(this)
-                .build()
-                .inject(this)
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
     }
 
     override fun activityInjector(): AndroidInjector<Activity> {
