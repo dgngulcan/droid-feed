4598396b725a1556247ba4d72ae59dab85f1ee2d
dgngulcan <dgngulcan@gmail.com>
2018-07-10 21:17:17 -0400

    add newsletter viewmodel binder

--- app/src/main/java/com/droidfeed/di/ViewModelModule.kt
+++ app/src/main/java/com/droidfeed/di/ViewModelModule.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProvider
 import com.droidfeed.ui.common.DFViewModelFactory
 import com.droidfeed.ui.module.feed.FeedViewModel
 import com.droidfeed.ui.module.main.MainViewModel
+import com.droidfeed.ui.module.newsletter.NewsletterViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -17,6 +18,9 @@ import dagger.multibindings.IntoMap
 abstract class ViewModelModule {
 
     @Binds
+    internal abstract fun bindViewModelFactory(factory: DFViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
     @IntoMap
     @ViewModelKey(MainViewModel::class)
     abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
@@ -27,6 +31,8 @@ abstract class ViewModelModule {
     abstract fun bindFeedViewModel(feedViewModel: FeedViewModel): ViewModel
 
     @Binds
-    internal abstract fun bindViewModelFactory(factory: DFViewModelFactory): ViewModelProvider.Factory
+    @IntoMap
+    @ViewModelKey(NewsletterViewModel::class)
+    abstract fun bindNewsLetterViewModel(newsletterViewModel: NewsletterViewModel): ViewModel
 
 }
