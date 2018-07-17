94e0144be91f4473ad1c50c7cf8238adf3590ef3
dgngulcan <dgngulcan@gmail.com>
2018-07-17 08:58:55 -0400

    remove view model provider

--- app/src/main/java/com/droidfeed/di/ViewModelModule.kt
+++ app/src/main/java/com/droidfeed/di/ViewModelModule.kt
@@ -1,8 +1,6 @@
 package com.droidfeed.di
 
 import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
-import com.droidfeed.ui.common.DFViewModelFactory
 import com.droidfeed.ui.module.feed.FeedViewModel
 import com.droidfeed.ui.module.main.MainViewModel
 import com.droidfeed.ui.module.newsletter.NewsletterViewModel
@@ -16,9 +14,6 @@ import dagger.multibindings.IntoMap
  */
 @Module
 abstract class ViewModelModule {
-
-    @Binds
-    internal abstract fun bindViewModelFactory(factory: DFViewModelFactory): ViewModelProvider.Factory
 
     @Binds
     @IntoMap
