df252f03c7c09c656b10c99df7a21bd08448c00c
dgngulcan <dgngulcan@gmail.com>
2018-08-23 07:29:06 -0400

    add post source filtering to dao

--- app/src/main/java/com/droidfeed/data/db/PostDao.kt
+++ app/src/main/java/com/droidfeed/data/db/PostDao.kt
@@ -2,12 +2,7 @@ package com.droidfeed.data.db
 
 import android.arch.lifecycle.LiveData
 import android.arch.paging.DataSource
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Delete
-import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy
-import android.arch.persistence.room.Query
-import android.arch.persistence.room.Update
+import android.arch.persistence.room.*
 import com.droidfeed.data.model.Post
 
 @Dao
@@ -23,21 +18,23 @@ interface PostDao {
     fun deleteArticle(article: Post)
 
     @Query(
-        "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "ORDER BY pub_date_timestamp DESC"
+            "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} WHERE source_id " +
+                    "IN (SELECT  ${AppDatabase.SOURCE_TABLE_NAME}.id from ${AppDatabase.SOURCE_TABLE_NAME} " +
+                    "WHERE is_active = 1) " +
+                    "ORDER BY pub_date_timestamp DESC"
     )
     fun getAllPosts(): DataSource.Factory<Int, Post>
 
     @Query(
-        "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "ORDER BY pub_date_timestamp DESC"
+            "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} " +
+                    "ORDER BY pub_date_timestamp DESC"
     )
     fun getAllPostsLiveData(): LiveData<List<Post>>
 
     @Query(
-        "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "WHERE bookmarked = 1 " +
-            "ORDER BY pub_date_timestamp DESC"
+            "SELECT * FROM ${AppDatabase.POST_TABLE_NAME} " +
+                    "WHERE bookmarked = 1 " +
+                    "ORDER BY pub_date_timestamp DESC"
     )
     fun getBookmarkedArticles(): LiveData<List<Post>>
 
@@ -45,23 +42,23 @@ interface PostDao {
     fun getFeedItemCount(): Int
 
     @Query(
-        "SELECT COUNT(*) FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "WHERE bookmarked = 1"
+            "SELECT COUNT(*) FROM ${AppDatabase.POST_TABLE_NAME} " +
+                    "WHERE bookmarked = 1"
     )
     fun getBookmarkedItemCount(): Int
 
     @Query(
-        "DELETE FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "WHERE bookmarked = 0 " +
-            "IN (SELECT pub_date_timestamp from ${AppDatabase.POST_TABLE_NAME} " +
-            "ORDER BY pub_date_timestamp DESC LIMIT 10)"
+            "DELETE FROM ${AppDatabase.POST_TABLE_NAME} " +
+                    "WHERE bookmarked = 0 " +
+                    "IN (SELECT pub_date_timestamp from ${AppDatabase.POST_TABLE_NAME} " +
+                    "ORDER BY pub_date_timestamp DESC LIMIT 10)"
     )
     fun trimCache()
 
     @Query(
-        "DELETE FROM ${AppDatabase.POST_TABLE_NAME} " +
-            "WHERE bookmarked == 0 " +
-            "AND channel_title LIKE :sourceUrl || '%'"
+            "DELETE FROM ${AppDatabase.POST_TABLE_NAME} " +
+                    "WHERE bookmarked == 0 " +
+                    "AND channel_title LIKE :sourceUrl || '%'"
     )
     fun clearNonBookmarkedSource(sourceUrl: String)
 }
