29070c381c19341ab8e3527aedea813f6aecbcf4
dgngulcan <dgngulcan@gmail.com>
2018-12-18 23:21:40 -0500

    increase db version for new schema

--- app/src/main/java/com/droidfeed/data/db/AppDatabase.kt
+++ app/src/main/java/com/droidfeed/data/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.droidfeed.data.model.Post
 import com.droidfeed.data.model.Source
 
-@Database(entities = [(Source::class), (Post::class)], version = 3)
+@Database(entities = [(Source::class), (Post::class)], version = 4)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
