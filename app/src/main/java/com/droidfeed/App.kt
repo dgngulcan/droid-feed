f8e89b610fce1bea595bbf74eac3b99009158318
dgngulcan <dgngulcan@gmail.com>
2018-12-23 22:39:40 -0500

    refactor intent instantiations suppress unused warning for remoteConfig

--- app/src/main/java/com/droidfeed/App.kt
+++ app/src/main/java/com/droidfeed/App.kt
@@ -7,7 +7,7 @@ import android.content.SharedPreferences
 import android.net.Uri
 import com.droidfeed.di.DaggerAppComponent
 import com.droidfeed.util.appOpenCount
-import com.droidfeed.util.logStackTrace
+import com.droidfeed.util.logException
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -49,25 +49,29 @@ class App : Application(), HasActivityInjector {
  * Intent for sending an email for contact purposes.
  */
 val contactIntent: Intent by lazy {
-    val intent = Intent(Intent.ACTION_SENDTO)
-    intent.data = Uri.parse("mailto:hi@droidfeed.io")
-    intent.putExtra(Intent.EXTRA_EMAIL, "hi@droidfeed.io")
-    intent.putExtra(Intent.EXTRA_SUBJECT, "About DroidFeed")
+    Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto:hi@droidfeed.io")
+        putExtra(Intent.EXTRA_EMAIL, "hi@droidfeed.io")
+        putExtra(Intent.EXTRA_SUBJECT, "About DroidFeed")
+        addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    }
 }
 
 /**
  * Intent for sharing the application.
  */
 val shareIntent: Intent by lazy {
-    val intent = Intent()
-    intent.action = Intent.ACTION_SEND
-    intent.putExtra(
-        Intent.EXTRA_TEXT,
-        "Check out DroidFeed! News feed for Android Developers" +
-                "\n\nhttps://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}&referrer=utm_source%3Din_app_share"
-    )
-    intent.type = "text/plain"
-    intent
+    Intent().apply {
+        action = Intent.ACTION_SEND
+        type = "text/plain"
+        putExtra(
+            Intent.EXTRA_TEXT,
+            "Check out DroidFeed! News feed for Android Developers" +
+                    "\n\nhttps://play.google.com/store/apps/details?id=" +
+                    "${BuildConfig.APPLICATION_ID}&referrer=utm_source%3Din_app_share"
+        )
+        addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    }
 }
 
 /**
@@ -75,12 +79,17 @@ val shareIntent: Intent by lazy {
  */
 val rateAppIntent: Intent by lazy {
     try {
-        Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + BuildConfig.APPLICATION_ID))
-    } catch (e: android.content.ActivityNotFoundException) {
-        logStackTrace(e)
         Intent(
             Intent.ACTION_VIEW,
-            Uri.parse("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID)
+            Uri.parse("market://details?id=" + BuildConfig.APPLICATION_ID)
+        )
+    } catch (e: android.content.ActivityNotFoundException) {
+        logException(e)
+        Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse(
+                "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID
+            )
         )
     }
 }
