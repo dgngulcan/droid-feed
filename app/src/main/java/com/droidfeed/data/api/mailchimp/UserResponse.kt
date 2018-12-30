29f0f5aec3454d977c3cc903ae803153723ae243
dgngulcan <dgngulcan@gmail.com>
2018-12-29 22:52:55 -0500

    rename the fields

--- app/src/main/java/com/droidfeed/data/api/mailchimp/UserResponse.kt
+++ app/src/main/java/com/droidfeed/data/api/mailchimp/UserResponse.kt
@@ -1,8 +1,15 @@
 package com.droidfeed.data.api.mailchimp
 
+import com.squareup.moshi.Json
+
 data class UserResponse(
     val id: String,
-    val email_address: String,
-    val unique_email_id: String,
+
+    @field:Json(name = "email_address")
+    val email: String,
+
+    @field:Json(name = "unique_email_id")
+    val emailId: String,
+
     val status: String
 )
