a24bc8384933bf8f9a99cc813ad6d7d966e59147
dgngulcan <dgngulcan@gmail.com>
2019-01-15 23:52:49 -0500

    move logic to source repo

--- app/src/main/java/com/droidfeed/ui/module/main/MainViewModel.kt
+++ app/src/main/java/com/droidfeed/ui/module/main/MainViewModel.kt
@@ -2,33 +2,44 @@ package com.droidfeed.ui.module.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.Transformations.map
 import com.droidfeed.data.model.Source
+import com.droidfeed.data.repo.PostRepo
 import com.droidfeed.data.repo.SourceRepo
 import com.droidfeed.ui.adapter.UiModelClickListener
-import com.droidfeed.ui.adapter.model.SourceUiModel
+import com.droidfeed.ui.adapter.model.SourceUIModel
 import com.droidfeed.ui.common.BaseViewModel
 import com.droidfeed.util.event.Event
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    sourceRepo: SourceRepo
+    sourceRepo: SourceRepo,
+    postRepo: PostRepo
 ) : BaseViewModel() {
 
-    private val result = MutableLiveData<List<SourceUiModel>>()
     val bookmarksEvent = MutableLiveData<Event<Boolean>>()
     val hideMenuEvent = MutableLiveData<Event<Unit>>()
 
-    val sourceUiModelData: LiveData<List<SourceUiModel>> =
-        Transformations.switchMap(sourceRepo.sources) { sourceList ->
-            result.value = sourceList.map { SourceUiModel(it, sourceClickListener) }
-            result
+    val sourceUIModelData: LiveData<List<SourceUIModel>> =
+        map(sourceRepo.getSources()) { sourceList ->
+            sourceList.map { source ->
+                SourceUIModel(source, sourceClickListener)
+            }
         }
 
     private val sourceClickListener = object : UiModelClickListener<Source> {
-        override fun onClick(model: Source) {
-            model.isActive = !model.isActive
-            sourceRepo.updateSource(model)
+        override fun onClick(source: Source) {
+            source.isActive = !source.isActive
+
+            launch(Dispatchers.IO) {
+                /* update source when activated */
+                if (source.isActive) {
+                    postRepo.refresh(listOf(source))
+                }
+                sourceRepo.updateSource(source)
+            }
         }
     }
 
