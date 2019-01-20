4857d2161ef7117e32f4dc8c2d717be99b2de441
dgngulcan <dgngulcan@gmail.com>
2019-01-19 23:06:29 -0500

    remove click listener tracking

--- app/src/main/java/com/droidfeed/ui/common/BaseViewModel.kt
+++ app/src/main/java/com/droidfeed/ui/common/BaseViewModel.kt
@@ -1,6 +1,5 @@
 package com.droidfeed.ui.common
 
-import android.os.SystemClock
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,21 +8,7 @@ import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel : ViewModel(), CoroutineScope {
 
-    private var lastClickTime: Long = 0
     private val job = Job()
-
-    /**
-     * Returns if a click event should be consumed or not. It is mainly to prevent spam clicks.
-     */
-    var canClick: Boolean = true
-        get() {
-            return if (SystemClock.elapsedRealtime() - lastClickTime < CLICK_TIMEOUT_IN_MILLIS) {
-                false
-            } else {
-                lastClickTime = SystemClock.elapsedRealtime()
-                true
-            }
-        }
 
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.Main
@@ -33,7 +18,4 @@ abstract class BaseViewModel : ViewModel(), CoroutineScope {
         job.cancel()
     }
 
-    companion object {
-        private const val CLICK_TIMEOUT_IN_MILLIS = 200
-    }
 }
