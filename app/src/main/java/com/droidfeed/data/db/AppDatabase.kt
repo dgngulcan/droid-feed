b0b27ec6d473df8c637ee6d5a61ec81bb398b5aa
dgngulcan <dgngulcan@gmail.com>
2019-01-19 23:11:51 -0500

    update codestyle

--- app/src/main/java/com/droidfeed/data/db/AppDatabase.kt
+++ app/src/main/java/com/droidfeed/data/db/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.RoomDatabase
 import com.droidfeed.data.model.Post
 import com.droidfeed.data.model.Source
 
-@Database(entities = [(Source::class), (Post::class)], version = 4)
+@Database(
+    entities = [(Source::class),
+        (Post::class)],
+    version = 4
+)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
