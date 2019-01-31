ca426640a97da2985022ccd3f6102339dac6382f
dgngulcan <dgngulcan@gmail.com>
2019-01-30 22:42:57 -0500

    add fade in and out animation extensions remove loadImage

--- app/src/main/java/com/droidfeed/util/extention/Viewx.kt
+++ app/src/main/java/com/droidfeed/util/extention/Viewx.kt
@@ -4,24 +4,12 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.droidfeed.R
+import com.droidfeed.util.AnimUtils
 
-/**
- * Loads given image url into the ImageView via Glide.
- *
- * @param urlOrResource
- */
-fun ImageView.loadImage(urlOrResource: Any) {
-    Glide.with(context)
-        .load(urlOrResource)
-        .apply(
-            RequestOptions()
-                .error(context.getDrawable(R.drawable.ic_broken_image_black_24dp))
-        )
-        .into(this)
-}
 
 /**
  * Hides the soft keyboard.
@@ -29,4 +17,27 @@ fun ImageView.loadImage(urlOrResource: Any) {
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun View.fadeIn(duration: Long = AnimUtils.SHORT_ANIM_DURATION) {
+    if (isVisible && alpha != 1f) {
+        animate().apply {
+            this.duration = duration
+            alpha(1f)
+            start()
+        }
+    }
+}
+
+fun View.fadeOut(
+    toAlpha: Float = 0f,
+    duration: Long = AnimUtils.SHORT_ANIM_DURATION
+) {
+    if (isVisible && alpha != toAlpha) {
+        animate().apply {
+            this.duration = duration
+            alpha(toAlpha)
+            start()
+        }
+    }
 }
