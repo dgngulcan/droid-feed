a6651e09f36b3fd958cb39479b2c52883d0d6cdc
dgngulcan <dgngulcan@gmail.com>
2019-02-02 21:14:32 -0500

    refactor code style

--- app/src/main/java/com/droidfeed/ui/module/contribute/ContributeFragment.kt
+++ app/src/main/java/com/droidfeed/ui/module/contribute/ContributeFragment.kt
@@ -4,28 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.droidfeed.databinding.FragmentContributeBinding
 import com.droidfeed.ui.common.BaseFragment
 import com.droidfeed.util.AnimUtils.Companion.MEDIUM_ANIM_DURATION
 import com.droidfeed.util.CustomTab
-import javax.inject.Inject
+import com.droidfeed.util.event.EventObserver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class ContributeFragment : BaseFragment("contribute") {
 
     private lateinit var binding: FragmentContributeBinding
-    private lateinit var viewModel: ContributeViewModel
+    private lateinit var contributeViewModel: ContributeViewModel
 
     @Inject
     lateinit var customTab: CustomTab
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProviders.of(
+        contributeViewModel = ViewModelProviders.of(
             this,
             viewModelFactory
         ).get(ContributeViewModel::class.java)
@@ -37,16 +37,28 @@ class ContributeFragment : BaseFragment("contribute") {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentContributeBinding.inflate(inflater, container, false)
-        binding.viewModel = viewModel
+        binding = FragmentContributeBinding.inflate(
+            inflater,
+            container,
+            false
+        ).apply {
+            viewModel = contributeViewModel
+            setLifecycleOwner(this@ContributeFragment)
+        }
 
-        init()
-        initObservers()
+        subscribeOpenRepositoryEvent()
+        initAnimations()
 
         return binding.root
     }
 
-    private fun init() {
+    private fun subscribeOpenRepositoryEvent() {
+        contributeViewModel.openRepositoryEvent.observe(viewLifecycleOwner, EventObserver { url ->
+            customTab.showTab(url)
+        })
+    }
+
+    private fun initAnimations() {
         binding.animView.setOnClickListener {
             if (!binding.animView.isAnimating) {
                 binding.animView.speed *= -1f
@@ -59,12 +71,5 @@ class ContributeFragment : BaseFragment("contribute") {
             delay(MEDIUM_ANIM_DURATION)
             binding.animView.resumeAnimation()
         }
-    }
-
-    private fun initObservers() {
-        viewModel.openRepositoryEvent.observe(viewLifecycleOwner,
-            Observer { url ->
-                url?.let { customTab.showTab(it) }
-            })
     }
 }
--- app/src/main/java/com/droidfeed/ui/module/contribute/ContributeViewModel.kt
+++ app/src/main/java/com/droidfeed/ui/module/contribute/ContributeViewModel.kt
@@ -1,15 +1,16 @@
 package com.droidfeed.ui.module.contribute
 
+import androidx.lifecycle.MutableLiveData
 import com.droidfeed.BuildConfig
 import com.droidfeed.ui.common.BaseViewModel
-import com.droidfeed.ui.common.SingleLiveEvent
+import com.droidfeed.util.event.Event
 import javax.inject.Inject
 
 class ContributeViewModel @Inject constructor() : BaseViewModel() {
 
-    val openRepositoryEvent = SingleLiveEvent<String>()
+    val openRepositoryEvent = MutableLiveData<Event<String>>()
 
     fun openRepository() {
-        openRepositoryEvent.setValue(BuildConfig.DROIDFEED_GITHUB_URL)
+        openRepositoryEvent.postValue(Event(BuildConfig.DROIDFEED_GITHUB_URL))
     }
 }
